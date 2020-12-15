da7c89ed9a01a808a77781577cf4c62ddd0c70f1
ShakibUddin <shakibuddinbhuiyan@gmail.com>
2020-12-15 20:46:04 +0600

    12/15/2020

--- app/src/main/java/com/sakibuddinbhuiyan/zikir/utils/Data.java
+++ app/src/main/java/com/sakibuddinbhuiyan/zikir/utils/Data.java
@@ -12,5 +12,10 @@ public class Data {
                 "A tree will be planted for you in Paradise. ", "\"সুবহানআল্লাহ বা সুবহানআল্লাহ ওয়া বিহামদিহি\n" +
                 "পুরষ্কার:\n" +
                 "                    \"তোমার জন্য জান্নাতে একটি গাছ লাগানো হবে।\"");
+
+        zikrData.put("Reciting Surah Ikhlas will give protection against every kind of affliction and calamity",
+                "সূরা ইখলাস তেলাওয়াত করলে সর্বপ্রকার কষ্ট ও বিপর্যয় থেকে রক্ষা পাওয়া যায়");
+        zikrData.put("Surah al Ikhlas is a means of forgiveness of sins","সূরা আল ইখলাস পাপ ক্ষমা করার একটি মাধ্যম");
+        zikrData.put("Surah Ikhlas is a means of eliminating poverty","সুরা ইখলাস দারিদ্র্য দূরীকরণের একটি মাধ্যম");
     }
 }
